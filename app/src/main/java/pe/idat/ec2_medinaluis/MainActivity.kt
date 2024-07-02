1dd8d03f5a00c8a7bad52fee5763e9847f65cbb1
Gherofx <pt46429444@idat.edu.pe>
2024-07-02 13:55:45 -0500

    creacion del proyecto

---
+++ app/src/main/java/pe/idat/ec2_medinaluis/MainActivity.kt
@@ -0,0 +1,36 @@
+package pe.idat.ec2_medinaluis
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pe.idat.ec2_medinaluis.ui.theme.EC2_MedinaLuisTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EC2_MedinaLuisTheme {
+
+            }
+        }
+    }
+}
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EC2_MedinaLuisTheme {
+
+    }
+}
